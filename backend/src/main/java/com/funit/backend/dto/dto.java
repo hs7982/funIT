fead22aa04afa4d88db907d3e6b862aef154640c
hseok <17452867+hs7982@users.noreply.github.com>
2024-04-02 16:13:07 +0900

    유저정보 불러오기

--- backend/src/main/java/com/funit/backend/dto/dto.java
+++
@@ -1,4 +0,0 @@
-package com.funit.backend.dto;
-
-public class dto {
-}
---
+++ backend/src/main/java/com/funit/backend/entity/User.java
@@ -0,0 +1,39 @@
+package com.funit.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private int id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "tel")
+    private String tel;
+
+    @Column(name = "permission", nullable = false, columnDefinition = "VARCHAR(20) DEFAULT 'user'")
+    private String permission;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "join_date", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Date joinDate;
+}
