7f213d724ea4e9fce368b3f457cc05ce44d2e4b5
hseok <17452867+hs7982@users.noreply.github.com>
2024-04-08 17:27:21 +0900

    영화 등록 기능 구현

--- backend/src/main/java/com/funit/backend/entity/Movie.java
+++ backend/src/main/java/com/funit/backend/entity/Movie.java
@@ -1,11 +1,11 @@
 package com.funit.backend.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,50 +14,47 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Builder
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "movie_id", updatable = false)
     private int id;
 
-    @Column(name = "producer", nullable = false)
+    @Column(name = "producer")
     private String producer;
 
     @Column(name = "title", nullable = false)
     private String title;
 
-    @ManyToOne(cascade = CascadeType.REMOVE)
+    @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id")
-    User user;
+    private User user;
 
-    @Column(name = "Detail")
-    private String Detail;
+    @Column(name = "detail")
+    private String detail;
 
     @Column(name = "target_credit", nullable = false)
-    private int target_credit;
+    private int targetCredit;
 
-    @Column(name = "status", nullable = false)
+    @Column(name = "status", nullable = false, columnDefinition = "INT DEFAULT 1")
     private int status; // 진행중, 성공, 실패
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "create_date", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private Date create_date;
+    @CreationTimestamp
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "modified_date")
-    private Date modified_date;
+    @UpdateTimestamp
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
 
-    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "end_date", nullable = false)
-    private Date end_date;
+    private LocalDateTime endDate;
 
     @ManyToMany
     @JoinTable(name = "movie_genre",
             joinColumns = @JoinColumn(name = "movie_id"),
             inverseJoinColumns = @JoinColumn(name = "genre_id"))
     private Set<Genre> genres = new HashSet<>();
-
-
-
 
 }
