09750a051fba17a39c656557ff3e6aecb25e9d7e
hseok <17452867+hs7982@users.noreply.github.com>
2024-04-08 17:27:21 +0900

    영화 등록 기능 구현

--- backend/src/main/java/com/funit/backend/controller/MovieController.java
+++ backend/src/main/java/com/funit/backend/controller/MovieController.java
@@ -1,13 +1,21 @@
 package com.funit.backend.controller;
 
+import com.funit.backend.dto.AddMovieRequest;
 import com.funit.backend.entity.Movie;
+import com.funit.backend.response.ResponseHandler;
 import com.funit.backend.service.MovieService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class MovieController {
     @Autowired
@@ -17,5 +25,21 @@ public class MovieController {
     public List<Movie> getAllMovies() {
         List<Movie> movie = movieService.getAllMovies();
         return movie;
+    }
+
+    @PostMapping("/movie/new")
+    public ResponseEntity<Object> addMovie(@RequestBody AddMovieRequest request) {
+        Movie savedMovie = movieService.save(request);
+
+        return (savedMovie != null) ?
+                ResponseHandler.responseBuilder(
+                    HttpStatus.CREATED,
+                    "영화 프로젝트가 생성되었습니다.",
+                    savedMovie) :
+                ResponseHandler.responseBuilder(
+                        HttpStatus.BAD_REQUEST,
+                        "잘못된 요청입니다.",
+                        null
+                );
     }
 }
