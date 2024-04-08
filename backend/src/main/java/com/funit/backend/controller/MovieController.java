8d5cf2c5956c5d954509bd78f7d56fef9dbf2529
hseok <17452867+hs7982@users.noreply.github.com>
2024-04-08 23:41:27 +0900

    충돌된 커밋 fix

--- backend/src/main/java/com/funit/backend/controller/MovieController.java
+++ backend/src/main/java/com/funit/backend/controller/MovieController.java
@@ -5,7 +5,6 @@ import com.funit.backend.dto.AddMovieRequestDTO;
 import com.funit.backend.entity.Movie;
 import com.funit.backend.response.ResponseHandler;
 import com.funit.backend.service.MovieService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,21 +45,5 @@ public class MovieController {
                 HttpStatus.CREATED,
                 "영화 프로젝트가 생성되었습니다.",
                 savedMovie);
-    }
-
-    @PostMapping("/movie/new")
-    public ResponseEntity<Object> addMovie(@RequestBody AddMovieRequest request) {
-        Movie savedMovie = movieService.save(request);
-
-        return (savedMovie != null) ?
-                ResponseHandler.responseBuilder(
-                    HttpStatus.CREATED,
-                    "영화 프로젝트가 생성되었습니다.",
-                    savedMovie) :
-                ResponseHandler.responseBuilder(
-                        HttpStatus.BAD_REQUEST,
-                        "잘못된 요청입니다.",
-                        null
-                );
     }
 }
