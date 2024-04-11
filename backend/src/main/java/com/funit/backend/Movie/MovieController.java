b148353863ce1c1b34517c6ec54d9c21d3575a70
rnjstn <rnjstn1213@naver.com>
2024-04-11 11:59:15 +0900

    영화 하나의 상세페이지 불러오기

--- backend/src/main/java/com/funit/backend/Movie/MovieController.java
+++ backend/src/main/java/com/funit/backend/Movie/MovieController.java
@@ -43,4 +43,14 @@ public class MovieController {
                 "영화 프로젝트가 생성되었습니다.",
                 savedMovie);
     }
+
+    @GetMapping("/{movie_id}")
+    public ResponseEntity<Object> findOne(@PathVariable int movie_id){
+        MovieEntity findOne = movieService.findOne(movie_id);
+
+        return ResponseHandler.responseBuilder(
+                HttpStatus.OK,
+                null,
+                findOne);
+    }
 }
