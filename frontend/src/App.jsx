e96c04dc06e5d8b07d08d5382dcab4d12052fc1b
gymhyun <joshuahong@naver.com>
2024-04-11 12:18:35 +0900

    검색창 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar.jsx";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import LogIn from "./pages/LogIn.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
       </Routes>
     </>
   );
