4c53452a4747b02069932950dd925b8f26d6ba96
hseok <17452867+hs7982@users.noreply.github.com>
2024-04-11 21:23:52 +0900

    notfound 페이지 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
 import Navbar from "./components/Navbar.jsx";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import LogIn from "./pages/LogIn.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LogIn />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<LogIn/>}/>
+
+                {/*찾는 페이지 없을때 (NotFound는 항상 최하단 위치!)*/}
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
