342ee7d5718c881f4bc80526d603a66e09df1a40
hseok <17452867+hs7982@users.noreply.github.com>
2024-04-12 10:10:56 +0900

    login페이지 update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,18 @@
 import Moviebox from "../components/Moviebox.jsx";
 
 const Home = () => {
-  return (
-    <div className="container mx-auto px-4">
-      <p className="text-3xl font-medium my-8">진행중인 펀딩</p>
-      <div className="flex flex-wrap justify-evenly gap-8">
-        <Moviebox />
-        <Moviebox />
-        <Moviebox />
-        <Moviebox />
-        <Moviebox />
-        <Moviebox />
-      </div>
-    </div>
-  );
+    return (
+        <div className="container mx-auto p-6">
+            <p className="text-3xl font-medium my-8">진행중인 펀딩</p>
+            <div className="flex flex-wrap justify-evenly gap-8">
+                <Moviebox/>
+                <Moviebox/>
+                <Moviebox/>
+                <Moviebox/>
+                <Moviebox/>
+                <Moviebox/>
+            </div>
+        </div>
+    );
 };
 export default Home;
