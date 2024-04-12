daa195e90f62edd27d21ab154306bed91aef3659
hseok <17452867+hs7982@users.noreply.github.com>
2024-04-12 16:30:16 +0900

    BE<->FE API 연동

--- backend/src/main/java/com/funit/backend/Like/LikeController.java
+++ backend/src/main/java/com/funit/backend/Like/LikeController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/** 좋아요 관련된 기능의 컨트롤러
- *
+/**
+ * 좋아요 관련된 기능의 컨트롤러
  */
 @RestController
-@RequestMapping("/likes")
+@RequestMapping("/api/likes")
 public class LikeController {
     @Autowired
     LikeService likeService;
@@ -21,7 +21,7 @@ public class LikeController {
      * 모든 좋아요 리스트를 반환합니다.
      */
     @GetMapping()
-    public ResponseEntity<Object> getAllLikes(){
+    public ResponseEntity<Object> getAllLikes() {
         List<LikeEntity> like = likeService.getAllLikes();
         return ResponseHandler.responseBuilder(
                 HttpStatus.OK,
@@ -29,8 +29,9 @@ public class LikeController {
                 like
         );
     }
+
     @GetMapping("/count")
-    public ResponseEntity<Object> getLikeCount(){
+    public ResponseEntity<Object> getLikeCount() {
         int count = likeService.countLike();
 
         return ResponseHandler.responseBuilder(
