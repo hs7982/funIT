f49a479b463590c416b9267246e35c18079041a7
hseok <17452867+hs7982@users.noreply.github.com>
2024-04-16 00:07:35 +0900

    영화 프로젝트 추가페이지 보완

--- frontend/src/components/GenreSelect.jsx
+++ frontend/src/components/GenreSelect.jsx
@@ -1,21 +1,11 @@
-import {Fragment, useEffect, useState} from 'react'
-import {Combobox, Transition} from '@headlessui/react'
-import {CheckIcon, ChevronUpDownIcon} from '@heroicons/react/20/solid'
+import {Fragment, useEffect, useState} from "react";
+import {Combobox, Transition} from "@headlessui/react";
+import {CheckIcon, ChevronUpDownIcon} from "@heroicons/react/20/solid";
 import axios from "axios";
 
-const people = [
-    {id: 1, name: 'Wade Cooper'},
-    {id: 2, name: 'Arlene Mccoy'},
-    {id: 3, name: 'Devon Webb'},
-    {id: 4, name: 'Tom Cook'},
-    {id: 5, name: 'Tanya Fox'},
-    {id: 6, name: 'Hellen Schmidt'},
-]
-
-const GenreSelect = () => {
-    const [query, setQuery] = useState('')
+const GenreSelect = (props) => {
     const [genres, setGenres] = useState([]);
-    const [selected, setSelected] = useState(genres[0])
+    const [selected, setSelected] = useState([]);
 
     const getGenres = async () => {
         const result = await axios({
@@ -24,97 +14,95 @@ const GenreSelect = () => {
             withCredentials: true,
             timeout: 5000,
         });
-
         return result.data;
-    }
+    };
 
-    const filteredGenre =
-        query === ''
-            ? genres
-            : genres.filter((genre) =>
-                genre.name
-                    .toLowerCase()
-                    .replace(/\s+/g, '')
-                    .includes(query.toLowerCase().replace(/\s+/g, ''))
-            )
+    const selectGenre = (select) => {
+        if (checkSelected(select)) {
+            // 이미 선택된 항목인 경우 배열에서 제거
+            setSelected((prevData) =>
+                prevData.filter((item) => item.id !== select.id)
+            );
+        } else {
+            // 선택되지 않은 항목인 경우 배열에 추가
+            setSelected((prevData) => [...prevData, select]);
+        }
+    };
+
+    const checkSelected = (target) => {
+        return !!selected.find((item) => item.id === target.id);
+    };
 
     useEffect(() => {
-        getGenres().then(r => setGenres(r.data));
+        getGenres().then((r) => setGenres(r.data));
     }, []);
 
-    return (
-        <>
+    useEffect(() => {
+        const selectedIds = selected.map(item => item.id);
+        props.updateGenre(selectedIds);
+    }, [selected]);
 
-            <Combobox value={selected} onChange={setSelected}>
-                <div className="relative mt-1">
-                    <div
-                        className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+    return (
+        <div>
+            <div className="relative mt-1">
+                <Combobox>
+                    <Combobox.Button className={"input input-bordered w-full"}>
                         <Combobox.Input
-                            className="input input-bordered w-full"
+                            className="w-full"
                             placeholder="장르를 선택해주세요."
-                            displayValue={(genre) => genre.name}
-                            onChange={(event) => setQuery(event.target.value)}
+                            displayValue={() => selected.map((sel) => sel.name).join(", ")}
+                            readOnly
                         />
-                        <Combobox.Button className="absolute inset-y-0 right-0 flex items-center px-2 input-bordered">
-                            <ChevronUpDownIcon
-                                className="h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                            />
-                        </Combobox.Button>
-                    </div>
+                    </Combobox.Button>
+                    <Combobox.Button className="absolute inset-y-0 right-0 flex items-center px-2 input-bordered">
+                        <ChevronUpDownIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                        />
+                    </Combobox.Button>
                     <Transition
                         as={Fragment}
                         leave="transition ease-in duration-100"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
-                        afterLeave={() => setQuery('')}
                     >
                         <Combobox.Options
                             className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
-                            {filteredGenre.length === 0 && query !== '' ? (
-                                <div className="relative cursor-default select-none px-4 py-2 text-gray-700">
-                                    Nothing found.
-                                </div>
-                            ) : (
-                                filteredGenre.map((genre) => (
-                                    <Combobox.Option
-                                        key={genre.id}
-                                        className={({active}) =>
-                                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                                                active ? 'bg-teal-600 text-white' : 'text-gray-900'
-                                            }`
-                                        }
-                                        value={genre}
-                                    >
-                                        {({selected, active}) => (
-                                            <>
-                        <span
-                            className={`block truncate ${
-                                selected ? 'font-medium' : 'font-normal'
-                            }`}
-                        >
-                          {genre.name}
-                        </span>
-                                                {selected ? (
-                                                    <span
-                                                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                                            active ? 'text-white' : 'text-teal-600'
-                                                        }`}
-                                                    >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true"/>
-                          </span>
-                                                ) : null}
-                                            </>
-                                        )}
-                                    </Combobox.Option>
-                                ))
-                            )}
+                            {genres.map((genre) => (
+                                <Combobox.Option
+                                    key={genre.id}
+                                    className={({active}) =>
+                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                                            (active || checkSelected(genre)) ? "bg-teal-600 text-white" : "text-gray-900"
+                                        }`
+                                    }
+                                    value={genre}
+                                    onClick={() => selectGenre(genre)}
+                                >
+                  <span
+                      className={`block truncate ${
+                          checkSelected(genre) ? "font-medium" : "font-normal"
+                      }`}
+                  >
+                    {genre.name}
+                  </span>
+                                    {checkSelected(genre) ? (
+                                        <span
+                                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                                checkSelected(genre) ? "text-white" : "text-teal-600"
+                                            }`}
+                                        >
+                      <CheckIcon className="h-5 w-5" aria-hidden="true"/>
+                    </span>
+                                    ) : null}
+                                </Combobox.Option>
+                            ))}
                         </Combobox.Options>
                     </Transition>
-                </div>
-            </Combobox>
-        </>
-    )
+                </Combobox>
+            </div>
+        </div>
+    );
 };
 
 export default GenreSelect;
