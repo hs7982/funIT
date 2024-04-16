0f8901e087a2f55caf2be40d94a39bfcbb016d32
gymhyun <joshuahong@naver.com>
2024-04-16 18:24:11 +0900

    펀딩 페이지 만들기, 프로젝트 생성 페이지 연결, 투자하기 페이지 연결

--- frontend/src/components/Moviebox.jsx
+++ frontend/src/components/Moviebox.jsx
@@ -1,25 +1,34 @@
-const Moviebox = () => {
+import {Link} from "react-router-dom";
+
+const Moviebox = ( {movie} ) => {
+
   return (
-    <div className="">
+    <div className="" key={movie.id}>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
           <img
             src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1h6j7k.img?w=640&h=457&m=6&x=159&y=174&s=359&d=94"
-            alt="파묘"
+            alt={movie.title}
           />
         </figure>
         <div className="p-4">
           <h2 className="card-title">
-            파묘
+            {movie.title}
             <div className="badge badge-secondary">NEW</div>
             <div className="grow"></div>
-            <div className="badge badge-outline">오컬트</div>
-            <div className="badge badge-outline">사회풍자</div>
+            {movie.genres.slice(0, 1).map(genre => (
+                <div key={genre.id} className="badge badge-outline">{genre.name}</div>
+            ))}
+            {movie.genres.length > 1 && (
+                <div className="badge bg-gray-100" style={{ fontSize: '16px' }}>+</div>
+            )}
           </h2>
           <p></p>
           <div className="pt-2 card-actions justify-end">
             <div className="pt-1 flex-1">110% 달성</div>
-            <button className="btn btn-sm bg-fuchsia-300">투자하기</button>
+            <Link to={"/funding/detail/" + movie.id}>
+              <button className="btn btn-sm bg-fuchsia-300">투자하기</button>
+            </Link>
           </div>
         </div>
       </div>
