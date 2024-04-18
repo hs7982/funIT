e56f30d768effc482e1b9560d86bf690ed70eaf8
gymhyun <joshuahong@naver.com>
2024-04-18 15:18:07 +0900

    회원가입 페이지 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar.jsx";
 import {Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import LogIn from "./pages/LogIn.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import NewMovieProject from "./pages/NewMovieProject.jsx";
 import MovieProject from "./pages/MovieProject.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
 
 function App() {
     return (
@@ -12,10 +13,11 @@ function App() {
             <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/login" element={<LogIn/>}/>
+                <Route path="/signin" element={<SignIn/>}/>
                 <Route path="/funding/new" element={<NewMovieProject/>}/>
                 <Route path="/funding" element={<MovieProject/>}/>
                 <Route path="/funding/detail/:id" element={<MovieProject/>}/>
+                <Route path="/signup" element={<SignUp/>}/>
 
                 {/*찾는 페이지 없을때 (NotFound는 항상 최하단 위치!)*/}
                 <Route path="*" element={<NotFound/>}/>
