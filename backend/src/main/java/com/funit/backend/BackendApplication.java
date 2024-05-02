f0aa54e70bf87f23210cd0c5bfde35558fad30f7
joobsoo1234 <junsu_01@naver.com>
2024-05-02 12:21:29 +0900

    이미지 업로드 기능 구현 완

--- backend/src/main/java/com/funit/backend/BackendApplication.java
+++ backend/src/main/java/com/funit/backend/BackendApplication.java
@@ -1,13 +1,19 @@
 package com.funit.backend;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		new SpringApplicationBuilder(BackendApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
+
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.properties,"
+			+ "classpath:aws.yml";
 
 }
