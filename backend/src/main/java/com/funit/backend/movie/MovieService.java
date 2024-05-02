e367c47b4a471b27b2d079d60fe257db55320e73
joobsoo1234 <junsu_01@naver.com>
2024-05-02 12:21:29 +0900

    이미지 업로드 기능 구현 완

--- backend/src/main/java/com/funit/backend/movie/MovieService.java
+++ backend/src/main/java/com/funit/backend/movie/MovieService.java
@@ -2,8 +2,10 @@ package com.funit.backend.movie;
 
 import com.funit.backend.movie.domain.Movie;
 import com.funit.backend.movie.domain.MovieRepository;
+import com.funit.backend.s3.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -11,12 +13,16 @@ import java.util.List;
 public class MovieService {
     @Autowired
     MovieRepository movieRepository;
+    @Autowired
+    ImageService imageService;
 
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
 
-    public Movie save(AddMovieRequestDTO request) {
+    public Movie save(AddMovieRequestDTO request, MultipartFile imageFile) {
+        String imgUrl = imageService.saveImage(imageFile);
+        request.setImageURL(imgUrl);
         return movieRepository.save(request.toEntity());
     }
 
