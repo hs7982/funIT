016bf493b0aa78be84e3f63affe693b1f1256de4
joobsoo1234 <junsu_01@naver.com>
2024-05-16 12:20:57 +0900

    회원가입 시 크레딧 생성(통장 생성) 투자시 펀딩 생성/펀딩테이블에 생성됨

---
+++ backend/src/main/java/com/funit/backend/funding/FundingDTO.java
@@ -0,0 +1,15 @@
+package com.funit.backend.funding;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FundingDTO {
+    private int fundingCmount;
+}
