168516702c78bd75881762bc539903113b8bf71d
hseok <17452867+hs7982@users.noreply.github.com>
2024-05-21 15:56:22 +0900

    마이페이지 레이아웃

--- frontend/src/pages/Mypage.jsx
+++ frontend/src/pages/Mypage.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import Moviebox from "../components/Moviebox.jsx";
 import axios from "axios";
 import {Link, useNavigate} from "react-router-dom";
@@ -47,13 +47,31 @@ const Mypage = () => {
                     </div>
 
                 ) : (
-                    <div className="flex flex-wrap justify-evenly gap-8">
-                        <div>
-                            {userInfo.name}
+                    <div className="flex flex-row">
+                        <div className="basis-1/2 pe-12 border-e">
+                            <div className="flex">
+                                <div className="rounded-full size-24 overflow-hidden me-3.5 shadow-lg">
+                                    <img
+                                        alt="프로필 사진"
+                                        src="/default-profile.png"
+                                        className="object-cover w-full h-full"
+                                    />
+                                </div>
+                                <div className="my-auto">
+                                    <p className="text-4xl font-bold">{userInfo.name}</p>
+                                    <p>{userInfo.email}</p>
+                                </div>
+                            </div>
+                            <div
+                                className="flex flex-col border rounded-3xl h-40 p-6 my-8 shadow-lg text-white bg-gradient-to-r from-rose-400 to-sky-300">
+                                <p className="text-lg font-medium">보유 크레딧</p>
+                                <p className="my-auto text-4xl font-medium">0원</p>
+                            </div>
                         </div>
-                        <div>
-                            {userInfo.email}
+                        <div className="basis-1/2 ps-12">
+                            ddd
                         </div>
+
                     </div>
                 )}
             </div>
