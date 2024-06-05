997e0303dc2895b05abe116ca22f32500c7df94a
hseok <17452867+hs7982@users.noreply.github.com>
2024-06-05 12:14:38 +0900

    영화 좋아요 기능 추가

--- backend/src/main/java/com/funit/backend/like/LikeService.java
+++ backend/src/main/java/com/funit/backend/like/LikeService.java
@@ -2,14 +2,20 @@ package com.funit.backend.like;
 
 import com.funit.backend.like.domain.LikeRepository;
 import com.funit.backend.like.domain.MovieLike;
+import com.funit.backend.movie.domain.Movie;
+import com.funit.backend.movie.domain.MovieRepository;
+import com.funit.backend.user.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class LikeService {
     @Autowired
     LikeRepository likeRepository;
+    @Autowired
+    MovieRepository movieRepository;
 
     public List<MovieLike> getAllLikes() {
         return likeRepository.findAll();
@@ -19,5 +25,29 @@ public class LikeService {
         return likeRepository.getCount(movieId);
     }
 
-    public List<MovieLike> likeOne(Integer movieId) { return likeRepository.findByMovieId(movieId);}
+    public List<MovieLike> likeOne(Integer movieId) {
+        return likeRepository.findByMovieId(movieId);
+    }
+
+    public MovieLike likeMovie(Integer movieId, User user) {
+        Movie movie = movieRepository.findById(movieId).orElseThrow(() -> new IllegalArgumentException("영화를 찾을 수 없습니다."));
+        if (likeRepository.findByMovieIdAndUserId(movieId, user.getId()) != null) {
+            throw new IllegalArgumentException("이미 해당 영화에 좋아요 하였습니다.");
+        }
+        return likeRepository.save(MovieLike.builder().movie(movie).user(user).build());
+    }
+
+    public void unlikeMovie(Integer movieId, User user) {
+        MovieLike like = likeRepository.findByMovieIdAndUserId(movieId, user.getId());
+        if (like == null) {
+            throw new IllegalArgumentException("해당 영화에 좋아요를 표시하지 않았습니다.");
+        }
+        likeRepository.delete(like);
+    }
+
+    public Boolean getLikeStatus(Integer movieId, User user) {
+        movieRepository.findById(movieId).orElseThrow(() -> new IllegalArgumentException("영화를 찾을 수 없습니다."));
+        MovieLike like = likeRepository.findByMovieIdAndUserId(movieId, user.getId());
+        return like != null;
+    }
 }
